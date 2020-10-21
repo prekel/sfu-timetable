f4198bd9d63de19099f0488d743e30456d51b7fa
moltenship7750 <moltenship7750@gmail.com>
2020-10-21 15:36:39 +0800

    добавлены вкладки

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -4,13 +4,30 @@
     v-if="isSelected"
     class="main-content"
   >
-    <day
-      v-for="i in 6"
-      :key="i"
-      :date="calcDate(i)"
-      :subjects="weekSubjects[i - 1]"
-      class="day"
-    />
+    <el-tabs>
+      <el-tab-pane
+        :label="getWeekTab(currentWeek)"
+      >
+        <day
+          v-for="i in 6"
+          :key="i"
+          :date="calcDate(i)"
+          :subjects="setSubjects(currentWeek)[i - 1]"
+          class="day-item"
+        />
+      </el-tab-pane>
+      <el-tab-pane
+        :label="getWeekTab(!currentWeek)"
+      >
+        <day
+          v-for="i in 6"
+          :key="i"
+          :date="calcDate(i + 7)"
+          :subjects="setSubjects(!currentWeek)[i - 1]"
+          class="day-item"
+        />
+      </el-tab-pane>
+    </el-tabs>
   </el-main>
 </template>
 
@@ -29,40 +46,83 @@ export default {
       target: {},
       isSelected: false,
       currentDate: null,
+      currentWeek: null
     }
   },
   computed: {
-    subjects() {
-      return getWeekSubjects(this.target.timetable, getWeekNum(this.currentDate))
-    },
     weekStart() {
       return getStartWeek(this.currentDate, this.currentDate.getDay())
     },
-    weekSubjects() {
-      return getSubjects(this.subjects)
-    }
   },
   mounted() {
     window.eventBus.$on("selectTarget", async (target) => {
       this.target = await getTarget(target)
       this.isSelected = true
       })
-    this.currentDate = new Date();
+    this.currentDate = new Date()
+    this.currentWeek = this.getCurrentWeek()
   },
   methods: {
     calcDate(indx) {
       return addDays(this.weekStart, indx);
+    },
+    getCurrentWeek() {
+      if (getWeekNum(this.currentDate) == 1) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    setSubjects(week) {
+      if (week) {
+       return getSubjects(getWeekSubjects(this.target.timetable, 2))
+      } else {
+       return getSubjects(getWeekSubjects(this.target.timetable, 1))
+      }
+    },
+    getWeekTab(week) {
+      if (week) {
+        return "Чётная"
+      } else {
+        return "Нечётная"
+      }
     }
   }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .main-content {
   padding: 0;
 }
 
-.day {
+.tab {
+  width: 50%;
+}
+
+.el-tabs__nav {
+  width: 100%;
+}
+
+.el-tabs__item {
+  color: white;
+  width: 50%;
+  text-align: center;
+}
+
+.el-tabs__nav-wrap::after {
+  background-color: $base-border;
+}
+
+.el-tabs__active-bar {
+  background-color: $primary;
+}
+
+.el-tabs__item.is-active {
+  color: $primary;
+}
+
+.day-item {
   margin-bottom: 20px;
 }
 </style>
